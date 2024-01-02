2407d44f1bc3dcea6f3c51ac774d4b50572ef1ce
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-01-02 20:09:23 +1000

    Fix release script

--- scripts/release.js
+++ scripts/release.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const semver = require('semver')
 
 const contentPath = './content.js'
-const manifestPath = './manifest.json'
+const manifestPaths = ['./manifest.json', './Safari/Shared (Extension)/Resources/manifest.json']
 const safariProjectPath = './safari/Control Panel for YouTube.xcodeproj/project.pbxproj'
 
 let releaseType = process.argv[2]
@@ -16,7 +16,7 @@ Usage:
   process.exit(1)
 }
 
-let currentVersion = JSON.parse(fs.readFileSync(manifestPath, {encoding: 'utf8'})).version
+let currentVersion = JSON.parse(fs.readFileSync(manifestPaths[0], {encoding: 'utf8'})).version
 let nextVersion = semver.inc(currentVersion, releaseType)
 
 fs.writeFileSync(
@@ -26,12 +26,14 @@ fs.writeFileSync(
   {encoding: 'utf8'}
 )
 
-fs.writeFileSync(
-  manifestPath,
-  fs.readFileSync(manifestPath, {encoding: 'utf8'})
-    .replace(/"version": "[^"]+"/, `"version": "${nextVersion}"`),
-  {encoding: 'utf8'}
-)
+for (let manifestPath of manifestPaths) {
+  fs.writeFileSync(
+    manifestPath,
+    fs.readFileSync(manifestPath, {encoding: 'utf8'})
+      .replace(/"version": "[^"]+"/, `"version": "${nextVersion}"`),
+    {encoding: 'utf8'}
+  )
+}
 
 fs.writeFileSync(
   safariProjectPath,
