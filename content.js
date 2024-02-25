ccbb35a6fe1588e9a2c0d0c00ddd5d2f9e7cc06b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-02-25 19:35:24 +1000

    Release v1.0.1
    Mobile version fixes and changes
    
    hideNextButton:
    - always hide the Next button on mobile, as it takes you to a random video, even if you just used the Previous button
    - (continue to only show the Previous button when it's enabled)
    
    hideSponsored:
    - fixed the ad immediately under mobile videos not being hidden
    
    hideSuggestedSections:
    - display the Hide suggested sections option on mobile, as it now applies to the mobile Home page
    - only try to hide sections on mobile when logged in, to avoid hiding the "Try searching to get started" home page you can get when browsing privately
    
    mobileGridView:
    - fixed non-video items in Search still being full-width
    - fixed watched/streamed/hidden channel videos not being hidden in Search when using the grid view

--- content.js
+++ content.js
@@ -6,7 +6,7 @@
 // @match       https://www.youtube.com/*
 // @match       https://m.youtube.com/*
 // @exclude     https://www.youtube.com/embed/*
-// @version     1
+// @version     2
 // ==/UserScript==
 let debug = false
 let debugManualHiding = false
