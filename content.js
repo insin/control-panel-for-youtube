693e346c9e84bc2d44cda3e2a9fd7b0624b6a185
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-07-31 17:38:24 +1000

    Release v1.2.1
    - Fixed hiding watched videos, streamed videos and VODs, and hidden channels in Home and Subscriptions on desktop

--- content.js
+++ content.js
@@ -6,7 +6,7 @@
 // @match       https://www.youtube.com/*
 // @match       https://m.youtube.com/*
 // @exclude     https://www.youtube.com/embed/*
-// @version     4
+// @version     5
 // ==/UserScript==
 let debug = false
 let debugManualHiding = false
