5b9dae6a4ae40a4535696e4ff698daef6021d1dd
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-11-03 11:25:35 +1000

    Release v1.3.0
    - Added an option to remove the new pink fade from the desktop progress bar
    - Hiding watched videos no longer applies to Search results

--- content.js
+++ content.js
@@ -6,7 +6,7 @@
 // @match       https://www.youtube.com/*
 // @match       https://m.youtube.com/*
 // @exclude     https://www.youtube.com/embed/*
-// @version     8
+// @version     9
 // ==/UserScript==
 let debug = false
 let debugManualHiding = false
