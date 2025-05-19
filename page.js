80987f4e217d746b050cef5ee978336fe0a0fb32
Jonny Buchanan <jonathan.buchanan@gmail.com>
2025-05-19 13:33:51 +1000

    Added support for all YouTube display languages in the "Always use original audio" feature

--- page.js
+++ page.js
@@ -22,26 +22,260 @@ function warn(...args) {
  * @type {Record<string, import("./types").PageLocale>}
  */
 const locales = {
+  'af-ZA': {
+    ORIGINAL: 'oorspronklike',
+  },
+  'am-ET': {
+    ORIGINAL: 'የመጀመሪያ',
+  },
+  'ar': {
+    ORIGINAL: 'أصلي',
+  },
+  'as-IN': {
+    ORIGINAL: 'মূল',
+  },
+  'az-Latn-AZ': {
+    ORIGINAL: 'orijinal',
+  },
+  'be-BY': {
+    ORIGINAL: 'арыгінальны',
+  },
+  'bg-BG': {
+    ORIGINAL: 'оригинален',
+  },
+  'bn-BD': {
+    ORIGINAL: 'মূল',
+  },
+  'bs-Latn-BA': {
+    ORIGINAL: 'original',
+  },
+  'ca-ES': {
+    ORIGINAL: 'original',
+  },
+  'cs-CZ': {
+    ORIGINAL: 'původní',
+  },
+  'da-DK': {
+    ORIGINAL: 'originalt',
+  },
+  'de-DE': {
+    ORIGINAL: 'Original',
+  },
+  'el-GR': {
+    ORIGINAL: 'πρωτότυπο',
+  },
   'en': {
     ORIGINAL: 'original',
   },
-  'fr': {
+  'es-419': {
     ORIGINAL: 'original',
+  },
+  'es-ES': {
+    ORIGINAL: 'original',
+  },
+  'es-US': {
+    ORIGINAL: 'original',
+  },
+  'et-EE': {
+    ORIGINAL: 'algne',
+  },
+  'eu-ES': {
+    ORIGINAL: 'jatorrizkoa',
+  },
+  'fa-IR': {
+    ORIGINAL: 'اصلی',
+  },
+  'fil-PH': {
+    ORIGINAL: 'orihinal',
+  },
+  'fr-CA': {
+    ORIGINAL: 'originale',
+  },
+  'fr-FR': {
+    ORIGINAL: 'original',
+  },
+  'gl-ES': {
+    ORIGINAL: 'orixinal',
+  },
+  'gu-IN': {
+    ORIGINAL: 'ઑરિજિનલ',
+  },
+  'he-IL': {
+    ORIGINAL: 'מקור',
+  },
+  'hi-IN': {
+    ORIGINAL: 'मूल',
+  },
+  'hr-HR': {
+    ORIGINAL: 'izvorno',
+  },
+  'hu-HU': {
+    ORIGINAL: 'eredeti',
+  },
+  'hy-AM': {
+    ORIGINAL: 'բնօրինակ',
+  },
+  'id-ID': {
+    ORIGINAL: 'asli',
+  },
+  'is-IS': {
+    ORIGINAL: 'upprunalegt',
+  },
+  'it-IT': {
+    ORIGINAL: 'originale',
   },
   'ja-JP': {
     ORIGINAL: 'オリジナル',
   },
+  'ka-GE': {
+    ORIGINAL: 'ორიგინალია',
+  },
+  'kk-KZ': {
+    ORIGINAL: 'түпнұсқа',
+  },
+  'km-KH': {
+    ORIGINAL: 'ដើម',
+  },
+  'kn-IN': {
+    ORIGINAL: 'ಮೂಲ',
+  },
+  'ko-KR': {
+    ORIGINAL: '원본',
+  },
+  'ky-KG': {
+    ORIGINAL: 'түпнуска',
+  },
+  'lo-LA': {
+    ORIGINAL: 'ຕົ້ນສະບັບ',
+  },
+  'lt-LT': {
+    ORIGINAL: 'pradinis',
+  },
+  'lv-LV': {
+    ORIGINAL: 'oriģināls',
+  },
+  'mk-MK': {
+    ORIGINAL: 'оригинален',
+  },
+  'ml-IN': {
+    ORIGINAL: 'ഒറിജിനൽ',
+  },
+  'mn-MN': {
+    ORIGINAL: 'эх хувь',
+  },
+  'mr-IN': {
+    ORIGINAL: 'मूळ',
+  },
+  'ms-MY': {
+    ORIGINAL: 'asal',
+  },
+  'my-MM': {
+    ORIGINAL: 'မူရင်း',
+  },
+  'nb-NO': {
+    ORIGINAL: 'original',
+  },
+  'ne-NP': {
+    ORIGINAL: 'मूल',
+  },
+  'nl-NL': {
+    ORIGINAL: 'Originele',
+  },
+  'or-IN': {
+    ORIGINAL: 'ମୂଳ',
+  },
+  'pa-Guru-IN': {
+    ORIGINAL: 'ਮੂਲ',
+  },
+  'pl-PL': {
+    ORIGINAL: 'oryginalny',
+  },
+  'pt-BR': {
+    ORIGINAL: 'original',
+  },
+  'pt-PT': {
+    ORIGINAL: 'original',
+  },
+  'ro-RO': {
+    ORIGINAL: 'original',
+  },
+  'ru-RU': {
+    ORIGINAL: 'оригинальная',
+  },
+  'si-LK': {
+    ORIGINAL: 'මුල්',
+  },
+  'sk-SK': {
+    ORIGINAL: 'pôvodná',
+  },
+  'sl-SI': {
+    ORIGINAL: 'Izvirnik',
+  },
+  'sq-AL': {
+    ORIGINAL: 'origjinale',
+  },
+  'sr-Cyrl-RS': {
+    ORIGINAL: 'оригинална',
+  },
+  'sr-Latn-RS': {
+    ORIGINAL: 'originalna',
+  },
+  'sw-TZ': {
+    ORIGINAL: 'halisi',
+  },
+  'ta-IN': {
+    ORIGINAL: 'அசல்',
+  },
+  'te-IN': {
+    ORIGINAL: 'అసలైనది',
+  },
+  'th-TH': {
+    ORIGINAL: 'เสียงต้นฉบับ',
+  },
+  'tr-TR': {
+    ORIGINAL: 'orijinal',
+  },
+  'uk-UA': {
+    ORIGINAL: 'оригінал',
+  },
+  'ur-PK': {
+    ORIGINAL: 'اصل',
+  },
+  'uz-Latn-UZ': {
+    ORIGINAL: 'original',
+  },
+  'vi-VN': {
+    ORIGINAL: 'gốc',
+  },
   'zh-Hans-CN': {
     ORIGINAL: '原始',
   },
+  'zh-Hant-HK': {
+    ORIGINAL: '原聲',
+  },
+  'zh-Hant-TW': {
+    ORIGINAL: '原文',
+  },
+  'zu-ZA': {
+    ORIGINAL: 'yokuqala',
+  },
 }
+
+const langCodes = lang.split('-')
+  .map((_, index, parts) => parts.slice(0, parts.length - index).join('-'))
+  .filter(langCode => Object.hasOwn(locales, langCode))
+  .concat('en')
 
 /**
  * @param {import("./types").PageLocaleKey} code
  * @returns {string}
  */
 function getString(code) {
-  return (locales[lang] || locales['en'])[code] || locales['en'][code];
+  for (let langCode of langCodes) {
+    if (Object.hasOwn(locales[langCode], code)) {
+      return locales[langCode][code]
+    }
+  }
 }
 
 async function alwaysUseOriginalAudio() {
@@ -68,12 +302,14 @@ async function alwaysUseOriginalAudio() {
     return
   }
 
+  let originalTrackName
   let originalTrack = tracks.find((track) => {
     for (let prop in track) {
       if (Object.prototype.toString.call(track[prop]) == '[object Object]' &&
           track[prop].id &&
           track[prop].name &&
           track[prop].name.includes(getString('ORIGINAL'))) {
+        originalTrackName = track[prop].name
         return true
       }
     }
@@ -90,7 +326,7 @@ async function alwaysUseOriginalAudio() {
     return
   }
 
-  log('alwaysUseOriginalAudio: setting original track', activeTrack, '→', originalTrack)
+  log('alwaysUseOriginalAudio: switching to original audio track', originalTrackName)
   // @ts-ignore
   $player.setAudioTrack?.(originalTrack)
 }
