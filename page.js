7e5c646592ae74103309879f9dda5a9138f29b50
KostromDan <kostromdan@gmail.com>
2026-07-31 16:58:16 +0300

    Fixed redirecting Shorts to the normal player on click

--- page.js
+++ page.js
@@ -5190,14 +5190,23 @@ function onDocumentClick(e) {
     }
   }
   if (desktop && config.redirectShorts) {
-    let $shortsLink = /** @type {HTMLAnchorElement} */ ($lastClickedElement?.closest('a[href^="/shorts/'))
+    let $shortsLink = /** @type {HTMLAnchorElement} */ ($lastClickedElement?.closest('a[href^="/shorts/"]'))
     if ($shortsLink) {
+      let videoId = $shortsLink.pathname.split('/').at(-1)
+      if (!videoId) return
+      let watchUrl = `/watch?v=${videoId}`
       // @ts-expect-error
-      let webCommandMetadata = $shortsLink._data?.commandMetadata?.webCommandMetadata
+      let navigationEndpoint = $shortsLink._data ?? $shortsLink.data ?? $shortsLink.closest('ytm-shorts-lockup-view-model')?.data?.onTap?.innertubeCommand
+      let webCommandMetadata = navigationEndpoint?.commandMetadata?.webCommandMetadata
+      log('redirecting Shorts video click to normal player')
+      $shortsLink.href = watchUrl
       if (webCommandMetadata) {
-        log('redirecting Shorts video click to normal player')
-        webCommandMetadata.url = `/watch?v=${$shortsLink.pathname.split('/').at(-1)}`
+        webCommandMetadata.url = watchUrl
         webCommandMetadata.webPageType = 'WEB_PAGE_TYPE_WATCH'
+      }
+      if (navigationEndpoint?.reelWatchEndpoint) {
+        navigationEndpoint.watchEndpoint = {videoId}
+        delete navigationEndpoint.reelWatchEndpoint
       }
     }
   }
